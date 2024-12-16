e2f7f2d0e4e2870e078f2e95b27608710ec1cc18
Sulman Ali <muhammad.sulman@ami-digital.com>
2024-12-16 14:01:26 +0500

    order table pagination and filter

--- resources/ts/components/OrdersTable.vue
+++ resources/ts/components/OrdersTable.vue
@@ -5,11 +5,12 @@ import prevent = event.prevent;
 
 const orderSelected = ref([]);
 const search = ref('')
+const statusSelectList = ref('')
 
 
 const columns = ref([
     { name: "routedStop", label: "Routed Stop", field: "routedStop", align: 'left'},
-    { name: "order", label: "Order", field: "order", align: 'left',  sortable: true },
+    { name: "order", label: "Order", field: "order", align: 'left',  sortable: true,},
     { name: "address", label: "Delivery Address", field: "address",  align: 'left', sortable: true },
     { name: "notes", label: "Delivery Notes", field: "notes",  align: 'left', sortable: true },
     { name: "status", label: "Status", field: "status",  align: 'left', sortable: true },
@@ -34,7 +35,7 @@ const rows = ref([
         id: 2,
         order: "#17084",
         address: "Ralphs Lane, PE21 7AX",
-        notes: "Orders: Explore how to tailor your view, manage bulk actions, and op",
+        notes: "Orders: Explore ur view, manage bulk actions, and op",
         contact: "+44 786 645 4587",
         date: "Dec 10, 2024",
         deliveryDate: "Dec 12, 2024",
@@ -42,22 +43,32 @@ const rows = ref([
     },
     {
         id: 3,
-        order: "#17084",
+        order: "#1484",
         address: "Ralphs Lane, PE21 7AX",
-        notes: "Orders: Explore how to tailor your view, manage bulk actions, and op",
+        notes: " Explore how to tailor your view, manage bulk actions, and op",
         contact: "+44 786 645 4587",
-        date: "Dec 10, 2024",
+        date: "Dec 30, 2024",
         deliveryDate: "Dec 12, 2024",
         name: "Gary King",
     },
 
     {
         id:4,
-        order: "#17084",
+        order: "#1784",
+        address: "Ralphs Lane, PE21 7AX",
+        notes: " how to tailor your view, manage bulk actions, and op",
+        contact: "+44 786 645 4587",
+        date: "Dec 20, 2024",
+        deliveryDate: "Dec 12, 2024",
+        name: "Gary King",
+    },
+    {
+        id:5,
+        order: "#17484",
         address: "Ralphs Lane, PE21 7AX",
         notes: "Orders: Explore how to tailor your view, manage bulk actions, and op",
         contact: "+44 786 645 4587",
-        date: "Dec 10, 2024",
+        date: "Dec 1, 2024",
         deliveryDate: "Dec 12, 2024",
         name: "Gary King",
     },
@@ -70,10 +81,38 @@ const statusOptions = ref([
     'Undelivered',
     'On Hold',
     'Completed',
-    'a'
 ]);
 
 const selectedStatus = ref<string[]>([]);
+const currentPage = ref<number>(1);
+const rowsPerPage = ref<number>(2);
+
+const currentPageRange = computed(() => {
+    const start = (currentPage.value - 1) * rowsPerPage.value + 1;
+    const end = Math.min(currentPage.value * rowsPerPage.value, rows.value.length);
+    return { start, end };
+});
+
+const isFilterDialogOpen = ref(false);
+const filters = ref<{ [key: string]: string }>({});
+
+const openFilterDialog = () => {
+    // Initialize filters object with empty values
+    filters.value = columns.value.reduce((acc, col) => {
+        acc[col.name] = "";
+        return acc;
+    }, {} as { [key: string]: string });
+
+    isFilterDialogOpen.value = true;
+};
+
+const applyDialogFilters = () => {
+    console.log("Applied Filters:", filters.value);
+    isFilterDialogOpen.value = false;
+};
+const resetFilters = () => {
+    filters.value = {};
+};
 
 const resetStatusFilters = () => {
     selectedStatus.value = [];
@@ -162,14 +201,27 @@ const applyFilters = () => {
                     padding="4px 14px"
                     class="px-2 bg-primary "
                     style="font-weight: normal; border-radius: 6px;"
+                    @click="openFilterDialog"
                 />
             </div>
         </div>
 
         <div>
-            <div class="flex items-center">
+            <div class="flex items-center no-wrap-lg">
+                <div class="mx-2 text-accent">
+                    {{ currentPageRange.start }}-{{ currentPageRange.end }} of {{ rows.length }}
+                </div>
                 <div>
-                    1-6 of 6
+                    <q-pagination
+                        v-model="currentPage"
+                        :max="Math.ceil(rows.length / rowsPerPage)"
+                        direction-links
+                        gutter="sm"
+                        active-design="outline"
+                        active-color="white"
+                        active-text-color="primary"
+                        class="text-primary"
+                    />
                 </div>
                 <q-btn
                     unelevated
@@ -188,12 +240,13 @@ const applyFilters = () => {
 
     <section id="order-table">
         <q-table
-            :rows="rows"
+            :rows="rows.slice((currentPage - 1) * rowsPerPage, currentPage * rowsPerPage)"
             :columns="columns"
             row-key="id"
             flat
             selection="multiple"
             color="primary"
+            hide-pagination
             v-model:selected="orderSelected"
             class="rounded-lg shadow-sm bg-white text-[#61646A]"
             table-header-class="bg-[#fafafa] font-semibold text-left"
@@ -202,12 +255,13 @@ const applyFilters = () => {
             <template v-slot:header-cell="props">
                 <q-th
                     :props="props"
-                    style="font-weight: 600; font-size: 14px;"
+                    style="font-weight: 600; font-size: 14px; min-width: 180px"
                     class="order-table-slot"
                 >
                     {{ props.col.label }}
                     <div class="float-right">
-                        <div  v-if="props.col.name  == 'status' ">
+                        <div  v-if="props.col.name  == 'routedStop'"></div>
+                        <div  v-else-if="props.col.name  == 'status' ">
                             <q-btn
                                 icon="filter_alt"
                                 unelevated
@@ -278,14 +332,14 @@ const applyFilters = () => {
                             >
                                 <q-menu anchor="bottom right" self="top right">
                                     <q-item>
-                                        <div class="flex search-input">
+                                        <div class="flex no-wrap search-input">
                                             <q-input v-model="search"
                                                      outlined
                                                      dense
                                                      class="text-gray-app min-w-[200px] bg-white"
                                                      :placeholder="`${props.col.label} Search`">
                                             </q-input>
-                                            <div class=" bg-primary p-2 flex items-center justify-center cursor-pointer" style="border-radius: 0 6px 6px 0">
+                                            <div class=" bg-primary p-2 flex no-wrap items-center justify-center cursor-pointer" style="border-radius: 0 6px 6px 0">
                                                 <q-icon size="14px" class="text-gray-app  inline-block" color="white" name="search"/>
                                                 <span class="text-white ml-1">search</span>
                                             </div>
@@ -308,6 +362,65 @@ const applyFilters = () => {
 
         </q-table>
     </section>
+
+    <q-dialog persistent v-model="isFilterDialogOpen">
+        <q-card class="w-[700px] max-w-[70vh] q-px-md q-py-md rounded-md" >
+
+            <q-card-section class="flex justify-between items-center no-wrap q-py-none">
+                <div class="text-lg font-semibold">Filters</div>
+                <q-btn
+                    flat
+                    round
+                    icon="close"
+                    class=" hover:text-red-700"
+                    v-close-popup
+                    unelevated
+                >
+                </q-btn>
+            </q-card-section>
+
+            <q-card-section>
+                <div v-for="col in columns" :key="col.name" class="mb-4">
+                    <div class="mb-1">
+                        <label>{{col.label}}</label>
+                    </div>
+
+                    <div>
+                        <q-select
+                            v-if="col.name  === 'status' "
+                            outlined
+                            dense
+                            v-model="statusSelectList"
+                            :options="statusOptions"
+                        />
+                        <q-input
+                            v-else
+                            v-model="filters[col.name]"
+                            dense
+                            outlined
+                            class="w-full"
+                        />
+                    </div>
+                </div>
+            </q-card-section>
+
+            <q-card-actions align="right">
+                <q-btn flat label="Reset" @click="resetFilters" />
+                <q-btn
+                    label="Apply Filters"
+                    unelevated
+                    dense
+                    filled
+                    text-color="white"
+                    size="md"
+                    padding="4px 14px"
+                    class="px-2 bg-primary "
+                    style="font-weight: normal; border-radius: 6px;"
+                    @click="applyDialogFilters"
+                />
+            </q-card-actions>
+        </q-card>
+    </q-dialog>
 </template>
 
 <style lang="scss">
