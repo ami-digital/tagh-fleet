89d34ddb583f5b6f1fe39d4d94df651760762b16
Ahmed Moh Afani <ahmed.mohammed@ami-digital.com>
2024-12-17 16:53:10 +0400

    merge locally

--- resources/ts/pages/Orders/View.vue
+++ resources/ts/pages/Orders/View.vue
@@ -2,7 +2,9 @@
 
 import MainLayout from "../../layouts/MainLayout.vue";
 import OrderViewDrawer from "../../components/orders/OrderViewDrawer.vue";
+import OrderViewDrawerCopy from "../../components/orders/OrderViewDrawerCopy.vue";
 
+const OrderView  = ref(false)
 const showModal = ref(false)
 const maximizedToggle = ref(true)
 </script>
@@ -10,9 +12,11 @@ const maximizedToggle = ref(true)
 <template>
 
         <q-page>
+            <q-btn label="OrderViewDrawer"   @click="OrderView = true" />
             <q-btn label="Open Full-Screen Modal"   @click="showModal = true" />
 
-             <OrderViewDrawerCopy @close="showModal = false" v-model="showModal" />
+             <OrderViewDrawerCopy @close="OrderView = false" v-model="OrderView" />
+             <OrderViewDrawer @close="showModal = false" v-model="showModal" />
         </q-page>
 
 </template>
