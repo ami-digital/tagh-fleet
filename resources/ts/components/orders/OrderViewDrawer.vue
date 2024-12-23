1f085250dd8f46b7355443c3597987eec93bfe80
Ahmed Moh Afani <ahmed.mohammed@ami-digital.com>
2024-12-23 14:54:30 +0400

    fixes

--- resources/ts/components/orders/OrderViewDrawer.vue
+++ resources/ts/components/orders/OrderViewDrawer.vue
@@ -49,149 +49,156 @@ const toggleDetails = () => {
 
       <section class="grid grid-cols-2">
 
-        <div class="p-5">
-          <div class="grid grid-cols-1 md:grid-cols-6 gap-y-4 md:gap-0 items-center">
+          <section class="max-sm:col-span-full">
+              <div class="p-5">
+                  <div class="grid grid-cols-1 md:grid-cols-6 gap-y-4 md:gap-0 items-center">
 
-            <!-- Location -->
-            <div class="text-[12px] font-medium text-gray-400 flex items-center">
-              <q-icon size="1.2rem" color="blue" class="mr-1" name="route" />
-              <span class="line-clamp-1 w-full md:w-9/12">south london/south coast</span>
-            </div>
+                      <!-- Location -->
+                      <div class="text-[12px] font-medium text-gray-400 flex items-center max-sm:col-span-6  max-sm:mb-2 ">
+                          <q-icon size="1.2rem" color="blue" class="mr-1" name="route" />
+                          <span class="line-clamp-1 w-full md:w-9/12">south london/south coast</span>
+                      </div>
 
-            <!-- Vehicle -->
-            <div class="text-[12px] font-medium text-gray-400 flex items-center">
-              <q-icon size="1.2rem" color="blue" class="mr-1" name="local_shipping" />
-              <span class="line-clamp-1 w-full md:w-9/12">KX68 XKD</span>
-            </div>
+                      <!-- Vehicle -->
+                      <div class="text-[12px] font-medium text-gray-400 flex items-center max-sm:col-span-6  max-sm:mb-2 ">
+                          <q-icon size="1.2rem" color="blue" class="mr-1" name="local_shipping" />
+                          <span class="line-clamp-1 w-full md:w-9/12">KX68 XKD</span>
+                      </div>
 
-            <!-- Driver -->
-            <div class="text-[12px] font-medium text-gray-400 flex items-center">
-              <q-icon size="1.2rem" color="blue" class="mr-1" name="person" />
-              <span class="line-clamp-1 w-full md:w-9/12">Bilal Hussain</span>
-            </div>
+                      <!-- Driver -->
+                      <div class="text-[12px] font-medium text-gray-400 flex items-center max-sm:col-span-6  max-sm:mb-2 ">
+                          <q-icon size="1.2rem" color="blue" class="mr-1" name="person" />
+                          <span class="line-clamp-1 w-full md:w-9/12">Bilal Hussain</span>
+                      </div>
 
-            <!-- Date -->
-            <div class="text-[12px] font-medium text-gray-400 flex items-center">
-              <q-icon size="1.2rem" color="blue" class="mr-1" name="calendar_today" />
-              <span class="line-clamp-1 w-full md:w-9/12">December 21</span>
-            </div>
+                      <!-- Date -->
+                      <div class="text-[12px] font-medium text-gray-400 flex items-center max-sm:col-span-6  max-sm:mb-2 ">
+                          <q-icon size="1.2rem" color="blue" class="mr-1" name="calendar_today" />
+                          <span class="line-clamp-1 w-full md:w-9/12">December 21</span>
+                      </div>
 
-            <!-- Buttons -->
-            <div class="col-span-1 md:col-span-2 flex flex-wrap justify-end gap-2">
+                      <!-- Buttons -->
+                      <div class="col-span-1 md:col-span-2 flex flex-wrap justify-end gap-2">
 
-                <q-btn
-                    dense
-                    unelevated
-                    color="secondary"
-                >
-                    <div  class="px-2">
-                        <q-icon size="1rem" name="edit" />
-                        <span class="text-[12px] ml-1">
+                          <q-btn
+                              dense
+                              unelevated
+                              color="secondary"
+                          >
+                              <div  class="px-2">
+                                  <q-icon size="1rem" name="edit" />
+                                  <span class="text-[12px] ml-1">
                               Edit Route
                            </span>
-                    </div>
-                </q-btn>
+                              </div>
+                          </q-btn>
 
-              <q-btn
-                  outline
-                  padding="xs"
-                  color="blue"
-                  icon="print"
-                  class="rounded-md"
-              ></q-btn>
+                          <q-btn
+                              outline
+                              padding="xs"
+                              color="blue"
+                              icon="print"
+                              class="rounded-md"
+                          ></q-btn>
 
-              <q-btn
-                  outline
-                  padding="xs"
-                  color="blue"
-                  icon="download"
-                  class="rounded-md"
-              />
-            </div>
-          </div>
-
-
-          <section class="mt-3">
-            <div class="p-4 border flex flex-col  justify-center w-full border-gray-200 rounded-md shadow-md">
-              <!-- Summary Information -->
-              <div class=" grid grid-cols-2 text-sm text-gray-500   ">
-
-                <div class="flex justify-between">
-                  <p>Completed: <span class="font-medium text-black">0/11</span></p>
-                  <p>Failed: <span class="font-medium text-black">0/11</span></p>
-                  <p>Outstanding: <span class="font-medium text-black">0/11</span></p>
-                </div>
-
-
-
-                <!-- Action Buttons -->
-                <div class="flex justify-end space-x-2">
-                  <q-icon name="delete" class="text-blue-500 cursor-pointer" size="1.2rem" />
-                </div>
-
-              </div>
-                <div class=" w-full flex justify-center">
-                    <q-btn
-                        unelevated
-                        dense
-                        color="blue"
-                        size="sm"
-                        class="rounded-md"
-                        @click="toggleDetails"
-                     >
-                        <div class="px-2">
-                            <q-icon name="keyboard_arrow_down" />
-                            <span>More</span>
-                        </div>
-                    </q-btn>
-                </div>
-
-              <div v-if="showDetails" class="mt-4 bg-blue-50 p-3 rounded-md">
-                <div class="grid grid-cols-2 gap-4 text-gray-600 text-sm">
-                  <div>
-                    <p>Started Time: <span class="font-semibold">Not Started</span></p>
-                    <p>Completed Time: <span class="font-semibold">Not Completed</span></p>
-                    <p>Sequence: <span class="font-semibold">Not Completed</span></p>
+                          <q-btn
+                              outline
+                              padding="xs"
+                              color="blue"
+                              icon="download"
+                              class="rounded-md"
+                          />
+                      </div>
                   </div>
-                  <div>
-                    <p>Planned Distance: <span class="font-semibold">580.98 Miles</span></p>
-                    <p>Actual Distance: <span class="font-semibold">-</span></p>
-                    <div class="flex items-center space-x-2">
-                      <span>Show Updated ETA:</span>
-                      <q-toggle v-model="showUpdatedETA" color="blue" />
-                    </div>
-                  </div>
-                </div>
-                <div class="flex justify-center mt-3">
-                  <q-btn
-                      unelevated
-                      dense
 
-                      icon="keyboard_arrow_up"
-                      color="blue"
-                      size="sm"
-                      class="rounded-md"
-                      @click="toggleDetails"
-                  />
-                </div>
+
+                  <section class="mt-3">
+                      <div class="p-4 border flex flex-col  justify-center w-full border-gray-200 rounded-md shadow-md">
+                          <!-- Summary Information -->
+                          <div class=" grid grid-cols-2 text-sm text-gray-500   ">
+
+                              <div class="flex justify-between">
+                                  <p>Completed: <span class="font-medium text-black">0/11</span></p>
+                                  <p>Failed: <span class="font-medium text-black">0/11</span></p>
+                                  <p>Outstanding: <span class="font-medium text-black">0/11</span></p>
+                              </div>
+
+
+
+                              <!-- Action Buttons -->
+                              <div class="flex justify-end space-x-2">
+                                  <q-icon name="delete" class="text-blue-500 cursor-pointer" size="1.2rem" />
+                              </div>
+
+                          </div>
+                          <div class=" w-full flex justify-center">
+                              <q-btn
+                                  unelevated
+                                  dense
+                                  color="blue"
+                                  size="sm"
+                                  class="rounded-md"
+                                  @click="toggleDetails"
+                              >
+                                  <div class="px-2">
+                                      <q-icon name="keyboard_arrow_down" />
+                                      <span>More</span>
+                                  </div>
+                              </q-btn>
+                          </div>
+
+                          <div v-if="showDetails" class="mt-4 bg-blue-50 p-3 rounded-md">
+                              <div class="grid grid-cols-2 gap-4 text-gray-600 text-sm">
+                                  <div>
+                                      <p>Started Time: <span class="font-semibold">Not Started</span></p>
+                                      <p>Completed Time: <span class="font-semibold">Not Completed</span></p>
+                                      <p>Sequence: <span class="font-semibold">Not Completed</span></p>
+                                  </div>
+                                  <div>
+                                      <p>Planned Distance: <span class="font-semibold">580.98 Miles</span></p>
+                                      <p>Actual Distance: <span class="font-semibold">-</span></p>
+                                      <div class="flex items-center space-x-2">
+                                          <span>Show Updated ETA:</span>
+                                          <q-toggle v-model="showUpdatedETA" color="blue" />
+                                      </div>
+                                  </div>
+                              </div>
+                              <div class="flex justify-center mt-3">
+                                  <q-btn
+                                      unelevated
+                                      dense
+
+                                      icon="keyboard_arrow_up"
+                                      color="blue"
+                                      size="sm"
+                                      class="rounded-md"
+                                      @click="toggleDetails"
+                                  />
+                              </div>
+                          </div>
+                      </div>
+                  </section>
+
+                  <section class="mt-2">
+                      <DashboardRoutesTable @view="() => emits('view')" />
+                  </section>
               </div>
-            </div>
           </section>
 
-          <section class="mt-2">
-            <DashboardRoutesTable @view="() => emits('view')" />
+
+          <section class="max-sm:col-span-full">
+              <div class="h-screen w-full">
+                  <iframe
+                      width="100%"
+                      height="100%"
+                      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3610.1715086696317!2d55.2794979!3d25.197438!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f69adc43dc605%3A0x4704117ce7a04ff2!2sDubai%20Mall%20Zabeel%20Parking%20-%20Upper%20Level!5e0!3m2!1sen!2sae!4v1734420073903!5m2!1sen!2sae"
+                      style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+              </div>
           </section>
-        </div>
 
 
-        <div class="h-screen w-full">
-          <iframe
-              width="100%"
-              height="100%"
-              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3610.1715086696317!2d55.2794979!3d25.197438!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f69adc43dc605%3A0x4704117ce7a04ff2!2sDubai%20Mall%20Zabeel%20Parking%20-%20Upper%20Level!5e0!3m2!1sen!2sae!4v1734420073903!5m2!1sen!2sae"
-              style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
-        </div>
       </section>
 
 
--- resources/ts/components/routes/RoutesDetailsDrawer.vue
+++ resources/ts/components/routes/RoutesDetailsDrawer.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 
-import DashboardRoutesTable from "../dashboard/DashboardRoutesTable.vue";
-import RoutsOrderItemsTable from "./RoutsOrderItemsTable.vue";
+
 import RoutesDetailsOrderInfoSection from "./RoutesDetailsOrderInfoSection.vue";
 import RoutesDetailsOrderDeliverySection from "./RoutesDetailsOrderDeliverySection.vue";
 
@@ -26,208 +25,116 @@ const emits = defineEmits<{
 const showDetails = ref(false);
 
 
-const showUpdatedETA = ref(false);
-
-
-const toggleDetails = () => {
-    showDetails.value = !showDetails.value;
-};
-
 
 </script>
 
 <template>
     <transition name="slide-up">
         <section v-if="model"
-                 class="side-drawer  text-gray-app overflow-auto absolute transform top-0 bottom-0    bg-white  w-full    ">
+                 class="side-drawer text-gray-app overflow-auto absolute transform top-0 bottom-0 bg-white w-full h-full">
 
             <div class="w-full relative">
                 <div class="absolute right-3 z-fullscreen top-3">
                     <q-btn round color="blue" icon="close" @click="() =>emits('close')"/>
-
                 </div>
             </div>
 
-            <section class="grid grid-cols-2 h-full overflow-y-auto relative">
+            <section class="grid grid-cols-1 md:grid-cols-2 h-full overflow-y-auto relative">
 
                 <div class="p-5">
                     <RoutesDetailsOrderDeliverySection/>
                     <RoutesDetailsOrderInfoSection/>
                 </div>
 
+                <!-- Responsive iframe -->
+                <section class="w-full">
+                    <div class="w-full p-5">
+                        <div class="h-[30vh] sm:h-[50vh]">
+                            <iframe
+                                width="100%"
+                                height="100%"
+                                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3610.1715086696317!2d55.2794979!3d25.197438!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f69adc43dc605%3A0x4704117ce7a04ff2!2sDubai%20Mall%20Zabeel%20Parking%20-%20Upper%20Level!5e0!3m2!1sen!2sae!4v1734420073903!5m2!1sen!2sae"
+                                style="border:0;" allowfullscreen="" loading="lazy"
+                                referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
 
-                <div class="  w-full p-5">
-                    <div class="h-[30vh]">
-                        <iframe
-                            width="100%"
-                            height="100%"
-                            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3610.1715086696317!2d55.2794979!3d25.197438!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f69adc43dc605%3A0x4704117ce7a04ff2!2sDubai%20Mall%20Zabeel%20Parking%20-%20Upper%20Level!5e0!3m2!1sen!2sae!4v1734420073903!5m2!1sen!2sae"
-                            style="border:0;" allowfullscreen="" loading="lazy"
-                            referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    </div>
-
-
-                    <div class="mt-5">
-                        <p class="text-center text-xl">Tracking Portal</p>
-                        <div class="flex items-center justify-center">
-                            <div
-                                class="border px-3  py-1 rounded-tl-lg rounded-bl-lg border-blue-500 text-blue-500   border-r-0">
-                                https://tracksr.io/5TzbVA
-                                <button
-                                    class="text-gray-500 hover:text-gray-700"
-                                >
-                                    📋
-                                </button>
-                            </div>
-                            <div
-                                class="border px-3  py-1 rounded-tr-lg rounded-br-lg border-blue-500 bg-blue-500   text-white  cursor-pointer hover:bg-blue-600  border-l-0">
-                                View Tracking Portal
+                        <div class="mt-5">
+                            <p class="text-center text-xl">Tracking Portal</p>
+                            <div class="flex items-center justify-center space-x-2">
+                                <div class="border px-3 py-1 rounded-tl-lg rounded-bl-lg border-blue-500 text-blue-500 border-r-0">
+                                    https://tracksr.io/5TzbVA
+                                    <button class="text-gray-500 hover:text-gray-700">📋</button>
+                                </div>
+                                <div class="border px-3 py-1 rounded-tr-lg rounded-br-lg border-blue-500 bg-blue-500 text-white cursor-pointer hover:bg-blue-600 border-l-0">
+                                    View Tracking Portal
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div class="mt-5">
-                        <p class="text-center text-xl">Notifications</p>
-                        <div class="bg-blue-100 shadow rounded-lg p-4">
-
-                            <div class="text-sm text-gray-600">
-
-                                <div class="flex items-center justify-between">
-                                    <p class="mb-1 text-black text-md">On Start Route SMS</p>
-                                    <div>
-                                        <p class="mb-0 text-xs text-right   ">Delivered</p>
-                                        <p class="mb-0 font-semibold  text-xs">07:38, December 20</p>
+                        <div class="mt-5">
+                            <p class="text-center text-xl">Notifications</p>
+                            <div class="bg-blue-100 shadow rounded-lg p-4">
+                                <div class="text-sm text-gray-600">
+                                    <div class="flex items-center justify-between">
+                                        <p class="mb-1 text-black text-md">On Start Route SMS</p>
+                                        <div>
+                                            <p class="mb-0 text-xs text-right">Delivered</p>
+                                            <p class="mb-0 font-semibold text-xs">07:38, December 20</p>
+                                        </div>
                                     </div>
+                                    <textarea readonly rows="3" class="w-full border border-gray-300 rounded px-3 py-2">
+                                        Hi, Your Driver is on the way with your order and will be delivered today.
+                                    </textarea>
                                 </div>
-                                <textarea
-                                    readonly
-                                    rows="3"
-                                    class="w-full border border-gray-300 rounded px-3 py-2"
-                                >Hi, Your Driver is on the way with your order and will be delivered today.</textarea>
                             </div>
                         </div>
+
                     </div>
-
-
-                </div>
-
-                <section class="absolute bottom-0 w-full    p-3">
-                    <div class="flex items-center justify-between">
-                        <div class="space-x-3">
-                            <q-btn
-                                dense
-                                outline
-                                color="secondary"
-
-                            >
-                                <div  class="px-2">
-                                    <q-icon size="1rem" name="print" />
-                                    <span class="text-[12px] ml-1">
-                              Print
-                           </span>
-                                </div>
-
-
-                            </q-btn>
-
-                            <q-btn
-                                dense
-                                outline
-                                color="secondary"
-                            >
-                                <div  class="px-2">
-                                    <q-icon size="1rem" name="print" />
-                                    <span class="text-[12px] ml-1">
-                              Packing Slip
-                           </span>
-                                </div>
-
-
-                            </q-btn>
-
-                            <q-btn
-                                dense
-                                outline
-                                color="secondary"
-                            >
-                                <div  class="px-2">
-                                    <q-icon size="1rem" name="print" />
-                                    <span class="text-[12px] ml-1">
-                              Print Label
-                           </span>
-                                </div>
-                            </q-btn>
-                            <q-btn
-                                dense
-                                unelevated
-
-                                color="secondary"
-                            >
-                                <div  class="px-2">
-                                    <q-icon size="1rem" name="edit" />
-                                    <span class="text-[12px] ml-1">
-                              Edit Stop
-                           </span>
-                                </div>
-                            </q-btn>
-                        </div>
-
-                        <div class="space-x-3">
-                            <q-btn
-                                dense
-                                outline
-                                color="red"
-                                style=" font-weight: normal "
-                            >
-                                <div  class="px-2">
-
-                                    <span class="text-[12px] ml-1">
-                              Close
-                           </span>
-                                </div>
-
-
-                            </q-btn>
-                            <q-btn
-                                dense
-                                unelevated
-                                color="secondary"
-                            >
-                                <div  class="px-2">
-
-                                    <span class="text-[12px] ml-1">
-                              Prev
-                           </span>
-                                </div>
-
-
-                            </q-btn>
-                            <q-btn
-                                dense
-                                unelevated
-                                color="secondary"
-                            >
-                                <div  class="px-2">
-
-                                    <span class="text-[12px] ml-1">
-                              Next
-                           </span>
-                                </div>
-
-
-                            </q-btn>
-
-                        </div>
-                    </div>
-
                 </section>
+
+                <!-- Bottom Buttons Section -->
+                <section class="absolute bottom-0 w-full p-3">
+                    <div class="flex items-center justify-between flex-wrap">
+                        <div class="space-x-3">
+                            <q-btn dense outline color="secondary" class="px-2">
+                                <q-icon size="1rem" name="print" />
+                                <span class="text-[12px] ml-1">Print</span>
+                            </q-btn>
+
+                            <q-btn dense outline color="secondary" class="px-2">
+                                <q-icon size="1rem" name="print" />
+                                <span class="text-[12px] ml-1">Packing Slip</span>
+                            </q-btn>
+
+                            <q-btn dense outline color="secondary" class="px-2">
+                                <q-icon size="1rem" name="print" />
+                                <span class="text-[12px] ml-1">Print Label</span>
+                            </q-btn>
+
+                            <q-btn dense unelevated color="secondary" class="px-2">
+                                <q-icon size="1rem" name="edit" />
+                                <span class="text-[12px] ml-1">Edit Stop</span>
+                            </q-btn>
+                        </div>
+
+                        <div class="space-x-3">
+                            <q-btn dense outline color="red" class="px-2">
+                                <span class="text-[12px] ml-1">Close</span>
+                            </q-btn>
+
+                            <q-btn dense unelevated color="secondary" class="px-2">
+                                <span class="text-[12px] ml-1">Prev</span>
+                            </q-btn>
+
+                            <q-btn dense unelevated color="secondary" class="px-2">
+                                <span class="text-[12px] ml-1">Next</span>
+                            </q-btn>
+                        </div>
+                    </div>
+                </section>
+
             </section>
-
-
-
-
         </section>
-
     </transition>
 </template>
 
@@ -254,5 +161,42 @@ const toggleDetails = () => {
 .side-drawer-enter-to, .side-drawer-leave {
     transform: translateY(0);
     opacity: 1; /* Ensure it becomes visible when entering */
+}
+
+/* Responsive Styles */
+@media (max-width: 768px) {
+    .side-drawer {
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        top: 0;
+        left: 0;
+    }
+
+    .grid {
+        grid-template-columns: 1fr;
+    }
+
+    .h-[30vh] {
+    height: 50vh;
+}
+
+    .p-5 {
+        padding: 1rem;
+    }
+
+    .flex {
+        flex-wrap: wrap;
+    }
+}
+
+@media (min-width: 768px) {
+    .h-[30vh] {
+    height: 30vh;
+}
+
+    .grid {
+        grid-template-columns: 1fr 1fr;
+    }
 }
 </style>
