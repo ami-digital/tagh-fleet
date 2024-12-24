a64676f4ae13b4384072558dff6ffdee08a2184b
Sulman Ali <muhammad.sulman@ami-digital.com>
2024-12-24 11:10:04 +0500

    page layout gridview

--- resources/ts/pages/LiveDashboard/Index.vue
+++ resources/ts/pages/LiveDashboard/Index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch } from "vue";
+import { ref, watch, computed } from "vue";
 import { Head } from "@inertiajs/vue3";
 import { Dark } from "quasar";
 import TeamsTable from "../../components/team-members/TeamsTable.vue";
@@ -15,6 +15,45 @@ watch(darkMode, (value) => {
 const search = ref('')
 const showModal = ref(false)
 const routeStatus = ref(false)
+
+const pageLayouts = [
+    { left: 'col-span-10', right: 'col-span-2' },
+    { left: 'col-span-9', right: 'col-span-3' },
+    { left: 'col-span-8', right: 'col-span-4' },
+    { left: 'col-span-7', right: 'col-span-5' },
+    { left: 'col-span-6', right: 'col-span-6' },
+    { left: 'col-span-5', right: 'col-span-7' },
+    { left: 'col-span-4', right: 'col-span-8' },
+    { left: 'col-span-3', right: 'col-span-9' },
+    { left: 'col-span-2', right: 'col-span-10' },
+];
+
+const pageLayoutIndex = ref(4);
+
+const gridLayout = ref({
+    left: pageLayouts[pageLayoutIndex.value].left,
+    right: pageLayouts[pageLayoutIndex.value].right,
+});
+
+const cycleLayout = (direction: 'left' | 'right') => {
+    const delta = direction === 'left' ? 1 : -1;
+    pageLayoutIndex.value = (pageLayoutIndex.value + delta + pageLayouts.length) % pageLayouts.length;
+    const { left, right } = pageLayouts[pageLayoutIndex.value];
+    gridLayout.value = { left, right };
+};
+
+const setFullLayout = (position: 'left' | 'right') => {
+    const index = position === 'left' ? 8 : 0;
+    const { left, right } = pageLayouts[index];
+    gridLayout.value = { left, right };
+    pageLayoutIndex.value = index;
+};
+
+const isLeftButtonDisabled = computed(() => pageLayoutIndex.value === pageLayouts.length - 1);
+const isRightButtonDisabled = computed(() => pageLayoutIndex.value === 0);
+
+
+
 </script>
 
 <template>
@@ -27,7 +66,7 @@ const routeStatus = ref(false)
 
             <section class="grid grid-cols-12">
 
-                <div class="p-5 col-span-8">
+                <div  class="p-5" :class="gridLayout.left">
 
                     <section class=" ">
                         <div class="p-4   flex flex-col  justify-center w-full  bg-blue-100     ">
@@ -52,12 +91,55 @@ const routeStatus = ref(false)
                 </div>
 
 
-                <div class="h-screen col-span-4">
+                <div class="relative h-screen"  :class="gridLayout.right">
                     <iframe
                         width="100%"
                         height="100%"
+                        class=""
                         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3610.1715086696317!2d55.2794979!3d25.197438!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f69adc43dc605%3A0x4704117ce7a04ff2!2sDubai%20Mall%20Zabeel%20Parking%20-%20Upper%20Level!5e0!3m2!1sen!2sae!4v1734420073903!5m2!1sen!2sae"
                         style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+
+
+                    <div class="absolute top-9 left-0 flex flex-col">
+                        <q-btn
+                            icon="keyboard_double_arrow_left"
+                            color="primary"
+                            class="mb-3"
+                            dense
+                            padding="6px"
+                            @click="setFullLayout('left')"
+                            :disable="isLeftButtonDisabled"
+                        />
+
+                        <q-btn
+                            icon="keyboard_arrow_left"
+                            color="primary"
+                            class="mb-3"
+                            padding="6px"
+                            @click="() => cycleLayout('left')"
+                            :disable="isLeftButtonDisabled"
+                        />
+
+                        <q-btn
+                            icon="keyboard_arrow_right"
+                            color="primary"
+                            class="mb-3"
+                            padding="6px"
+                            @click="() => cycleLayout('right')"
+                            :disable="isRightButtonDisabled"
+                        />
+
+                        <q-btn
+                            icon="keyboard_double_arrow_right"
+                            color="primary"
+                            class="mb-3"
+                            dense
+                            padding="6px"
+                            @click="setFullLayout('right')"
+                            :disable="isRightButtonDisabled"
+                        />
+                    </div>
 
                 </div>
             </section>
